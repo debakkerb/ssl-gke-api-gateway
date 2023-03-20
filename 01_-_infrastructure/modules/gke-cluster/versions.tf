72d1026c11fd75475408a853f98ce23f50d2716d
Bjorn De Bakker <debakkerbjorn@gmail.com>
2023-03-20 11:50:26 +0100

    Updating version files, giving more freedom in which versions to choose from

--- 01_-_infrastructure/modules/gke-cluster/versions.tf
+++ 01_-_infrastructure/modules/gke-cluster/versions.tf
@@ -15,17 +15,17 @@
  */
 
 terraform {
-  required_version = "= 1.4"
+  required_version = ">= 1.3.1"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "= 4.57.0"
+      version = ">= 4.55.0"
     }
 
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "= 4.57.0"
+      version = ">= 4.55.0"
     }
   }
 }
